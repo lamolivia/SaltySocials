657725f7ad9040819934ee74c4fd36d21c61b4c8
lamolivia <70666231+lamolivia@users.noreply.github.com>
2022-03-06 09:57:04 -0800

    views with example data

--- saltysocials/views.py
+++ saltysocials/views.py
@@ -11,16 +11,14 @@ def rec_view(request, **kwargs):
     # term = kwargs.get('term').upper() # for when front end can send to us
     # keywords = OPTIONS.get(term)
 
-    # approved_tweets = approved_tweets(keywords)
+    keywords = OPTIONS.get('love')
 
-
-    keywords = OPTIONS.get('LOVE')
-    approved_tweets = [['id', 'hello this is a tweet', 'mar 5 2022']]
+    tweets = approved_tweets(keywords)
 
     # will return tweet info consisting of twitter user,
     # profile photo, date of tweet and tweet text
     context = {
-        'tweets': approved_tweets,
+        'tweets': tweets,
     }
 
     return render(request, 'rec.html', context)
